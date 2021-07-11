af5865cdca7c0933e4c2ba30703c90395ab81991
Tri Hartanto <tri@trihartanto.com>
2021-07-11 11:30:30 +0700

    feat: test utility code forked from gitlab

---
+++ envgo.go
@@ -0,0 +1,120 @@
+package envgo
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const defaultDelimiter string = ":"
+
+func LoadDotEnv(filenames ...string) error {
+	err := godotenv.Load(filenames...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetString(path string, predefined string) string {
+	res := os.Getenv(path)
+	if res != "" {
+		return res
+	}
+	return predefined
+}
+
+func GetStringSlice(path string, delimiter string) []string {
+	res := make([]string, 0)
+	getString := GetString(path, "")
+	if getString == "" {
+		return res
+	}
+	// set default delimiter if empty
+	if delimiter == "" {
+		delimiter = defaultDelimiter
+	}
+	return strings.Split(getString, delimiter)
+}
+
+func GetInt(path string, predefined int) int {
+	res := os.Getenv(path)
+	if res != "" {
+		ires, err := strconv.Atoi(res)
+		if err == nil {
+			return ires
+		}
+	}
+	return predefined
+}
+
+func GetIntSlice(path string, delimiter string) []int {
+	res := make([]int, 0)
+	getInt := os.Getenv(path)
+	if getInt == "" {
+		return res
+	}
+	// set default delimiter if empty
+	if delimiter == "" {
+		delimiter = defaultDelimiter
+	}
+	getIntSlice := strings.Split(getInt, delimiter)
+	if len(getIntSlice) > 0 {
+		for _, v := range getIntSlice {
+			ires, err := strconv.Atoi(v)
+			if err == nil {
+				res = append(res, ires)
+			}
+		}
+	}
+	return res
+}
+
+func GetInt64(path string, predefined int64) int64 {
+	res := os.Getenv(path)
+	if res != "" {
+		ires, err := strconv.ParseInt(res, 10, 64)
+		if err == nil {
+			return ires
+		}
+	}
+	return predefined
+}
+
+func GetFloat32(path string, predefined float32) float32 {
+	res := os.Getenv(path)
+	if res != "" {
+		if f64, err := strconv.ParseFloat(res, 32); err == nil {
+			return float32(f64)
+		}
+	}
+	return predefined
+}
+
+func GetFloat64(path string, predefined float64) float64 {
+	res := os.Getenv(path)
+	if res != "" {
+		if f64, err := strconv.ParseFloat(res, 64); err == nil {
+			return f64
+		}
+	}
+	return predefined
+}
+
+func GetBool(path string, predefined bool) bool {
+	res := os.Getenv(path)
+	if res != "" {
+		return res == "true"
+	}
+	return predefined
+}
+
+func GetPort(path string, predefined int) string {
+	res := GetInt(path, predefined)
+	if res > 0 {
+		return fmt.Sprintf(":%d", res)
+	}
+	return fmt.Sprintf(":%d", predefined)
+}
---
+++ envgo_test.go
@@ -0,0 +1,134 @@
+package envgo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGoEnv_LoadDotEnv(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+}
+
+func TestGoEnv_GetString(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	str := GetString("TEST_GET_STRING", "")
+	if str != "Loaded Correctly" {
+		t.Errorf("Not Match (%s is wrong value)", str)
+	}
+}
+
+func TestGoEnv_GetStringSlice(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	slc := [3]string{"ValueOne", "ValueTwo", "ValueThree"}
+	// test with default delimiter
+	slice := GetStringSlice("TEST_GET_STRING_SLICE", "")
+	for k, v := range slc {
+		if slice[k] != v {
+			t.Errorf("Not Match (%s is wrong value for %s)", slice[k], v)
+		}
+	}
+	// test with custom delimiter
+	slice = GetStringSlice("TEST_GET_STRING_SLICE_DELIMITER", ",")
+	for k, v := range slc {
+		if slice[k] != v {
+			t.Errorf("Not Match (%s is wrong value for %s)", slice[k], v)
+		}
+	}
+}
+
+func TestGoEnv_GetInt(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	integer := GetInt("TEST_GET_NUMBER", 0)
+	if integer != 1234567890 {
+		t.Errorf("Not Match (%d is wrong value)", integer)
+	}
+}
+
+func TestGoEnv_GetIntSlice(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	slc := [5]int{1, 2, 3, 4, 5}
+	// test with default delimiter
+	slice := GetIntSlice("TEST_GET_NUMBER_SLICE", "")
+	for k, v := range slc {
+		if slice[k] != v {
+			t.Errorf("Not Match (%d is wrong value for %d)", slice[k], v)
+		}
+	}
+	// test with custom delimiter
+	slice = GetIntSlice("TEST_GET_NUMBER_SLICE_DELIMITER", ",")
+	for k, v := range slc {
+		if slice[k] != v {
+			t.Errorf("Not Match (%d is wrong value for %d)", slice[k], v)
+		}
+	}
+}
+
+func TestGoEnv_GetInt64(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	integer := GetInt64("TEST_GET_NUMBER", 0)
+	if integer != int64(1234567890) {
+		t.Errorf("Not Match (%d is wrong value)", integer)
+	}
+}
+
+func TestGoEnv_GetFloat32(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	f64 := GetFloat32("TEST_GET_NUMBER", 0)
+	if f64 != float32(1234567890) {
+		t.Errorf("Not Match (%f is wrong value)", f64)
+	}
+}
+
+func TestGoEnv_GetFloat64(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	f64 := GetFloat64("TEST_GET_NUMBER", 0)
+	if f64 != float64(1234567890) {
+		t.Errorf("Not Match (%f is wrong value)", f64)
+	}
+}
+
+func TestGoEnv_GetBool(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	isTrue := GetBool("TEST_GET_BOOL", true)
+	if isTrue == false {
+		t.Errorf("Not Match (%v is wrong value)", isTrue)
+	}
+}
+
+func TestGoEnv_GetPort(t *testing.T) {
+	err := LoadDotEnv()
+	if err != nil {
+		t.Errorf("Error (%s)", err.Error())
+	}
+	port := GetPort("TEST_GET_PORT", 0)
+	if port != fmt.Sprintf(":8080") {
+		t.Errorf("Not Match (%v is wrong value)", port)
+	}
+}
